8dc7b7568b39785cd6c2abd702ac250ebd977a82
Piyush Kumar Singh <91693521+piyushrajput0@users.noreply.github.com>
2022-10-08 18:45:57 +0530

    Time: 791 ms (54.51%), Space: 69.2 MB (56.87%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,101 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        
+        
+        int n=words.size();
+         int maxlen=words[0].size();
+        
+         vector<int>ans;
+        cout<<n*maxlen<<" "<<s.size()<<endl;
+        if(n*maxlen>s.size())
+        {
+            return ans;
+        }
+       
+        unordered_map<string,int>mpofwords;
+        for(int i=0;i<n;i++)
+        {
+            mpofwords[words[i]]++;
+        }
+        
+       
+        string str="";
+        for(int i=0;i<=(s.size()-n);i++)
+        {
+            str+=s[i];
+            
+            if(str.size()==maxlen)
+            {
+                
+                if(mpofwords.find(str)!=mpofwords.end())
+                {
+                    
+                    string frwords="";
+                    int rwords=1;
+                    unordered_map<string,int>copyofmap=mpofwords;
+                     copyofmap[str]--;
+                    if(copyofmap[str]==0)
+                    {
+                        copyofmap.erase(str);
+                    }
+                    if(copyofmap.size()==0)
+                    {
+                         ans.push_back(i-maxlen+1);
+                        str="";
+                        continue;
+                            
+                    }
+                    for(int j=i+1;j<s.size();j++)
+                    {
+                        
+                        frwords+=s[j];
+                       
+                        
+                        if(frwords.size()==maxlen)
+                        {
+                            if(copyofmap.find(frwords)!=mpofwords.end())
+                            {
+                                    copyofmap[frwords]--;
+                                    if(copyofmap[frwords]==0)
+                                    {
+                                        copyofmap.erase(frwords);
+                                    }
+
+                                   frwords="";
+                            }
+                            else
+                            {
+                                break;
+                            }
+                                
+                                
+                            
+                        }
+                        
+                        
+                        if(copyofmap.size()==0)
+                        {
+                           
+                            ans.push_back(i-maxlen+1);
+                            
+                                break;
+                            
+                        }
+                    }
+                    
+                    
+                }
+               
+                    str=str.substr(1);
+                
+                  
+               
+                
+                
+            }
+            
+        }
+        return ans;
+    }
+};
