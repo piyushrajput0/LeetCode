e2815480776574f7a06816565c5b79de4ae63464
Piyush Kumar Singh <91693521+piyushrajput0@users.noreply.github.com>
2022-10-19 23:34:23 +0530

    Time: 3 ms (92.95%), Space: 7.4 MB (32.20%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -52,14 +52,7 @@ public:
                     
             }
         }
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
+       
         
         return dp[n][m];
         
